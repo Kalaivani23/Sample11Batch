f48d98b1125ce32907c808513843451e7ecaf0b5
Kalaivani23 <kalaivanivelusamy07@gmail.com>
2019-11-19 11:34:50 +0530

    Kavitha add

--- src/emp/Employee.java
+++ src/emp/Employee.java
@@ -3,5 +3,9 @@ package emp;
 public class Employee {
 	public static void main(String[] args) {
 		System.out.println("Hi");
+		System.out.println("Add");
+		int a1 = 10;
+		int b1 = 20;
+		System.out.println(a1 + b1);
 	}
 }
